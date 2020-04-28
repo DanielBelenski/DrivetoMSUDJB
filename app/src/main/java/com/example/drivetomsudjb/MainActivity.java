8f5a6fc300d5db86aa217b8be1ca4cb64c9f281c
DanielBelenski <60483819+DanielBelenski@users.noreply.github.com>
2020-04-28 16:32:06 -0400

    Comments

--- app/src/main/java/com/example/drivetomsudjb/MainActivity.java
+++ app/src/main/java/com/example/drivetomsudjb/MainActivity.java
@@ -13,12 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //hide the support bar that displays the app title
         getSupportActionBar().hide();
     }
+    //method used by the button to open google maps
     public void startMaps(View view){
+        //creating object to hold the destination information and pass it to google maps
         Uri gmmIntentUri = Uri.parse("google.navigation:q=1+Normal+Ave,+Montclair+New+Jersey&mode=d&avoid=f");
+        //assigning the URI object to a intent
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        //telling the intent to change to google maps
         mapIntent.setPackage("com.google.android.apps.maps");
+        //starting google maps
         startActivity(mapIntent);
     }
 }

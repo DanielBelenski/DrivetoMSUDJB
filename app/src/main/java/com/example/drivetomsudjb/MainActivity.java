9dde3853f0bc4dfcee645ee5236b3322082ea4f4
DanielBelenski <60483819+DanielBelenski@users.noreply.github.com>
2020-04-28 16:21:52 -0400

    Finished app
    Sorry, I forgot to create the repository until after I finished the project.

---
+++ app/src/main/java/com/example/drivetomsudjb/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.drivetomsudjb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+    public void startMaps(View view){
+        Uri gmmIntentUri = Uri.parse("google.navigation:q=1+Normal+Ave,+Montclair+New+Jersey&mode=d&avoid=f");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+    }
+}
